a33b6abffa143635f9b7a9cfc24f5dd4f4585a13
arelys-granados <arelys-granados@users.noreply.github.com>
2019-05-03 17:55:31 -0600

    Inicio

--- OCFG/OCFG/Models/OCFG_Data/OCFG_DataBase.Designer.cs
+++ OCFG/OCFG/Models/OCFG_Data/OCFG_DataBase.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\DINADECO\OCFG\OCFG\Models\OCFG_Data\OCFG_DataBase.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Arelys Granados\DINADECO\OCFG\OCFG\Models\OCFG_Data\OCFG_DataBase.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
